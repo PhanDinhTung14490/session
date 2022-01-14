7fb6eff2732e23c771d3e7090e4354c84275f47e
Hanh20021706 <hanhs8155@gamil.com>
2022-01-14 23:41:09 +0700

    update

--- src/components/admin/newsList.js
+++ src/components/admin/newsList.js
@@ -8,6 +8,22 @@ const NewsList = {
     runder() {
         return /* html */`
         <div> ${NavAdmin.runder()} </div>
+        <div class ="flex my-8 justify-between "> 
+            <h2 class = "font-sans text-2xl font-bold uppercase"> quản lý tin tức </h2> 
+        
+       <div class =""> 
+            <a href="/admin/add" class="sm:ml-3">
+            <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <!-- Heroicon name: solid/check -->
+                <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                </svg>
+                Thêm Mới
+            </button>
+            </a>
+       </div>
+       </div>
+       
         <div class="max-w-5xl mx-auto">
         <div class="-my-0 sm:-mx-6 lg:-mx-8">
           <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
@@ -85,15 +101,6 @@ const NewsList = {
             </div>
           </div>
 
-        <a href="/admin/add" class="sm:ml-3">
-        <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-          <!-- Heroicon name: solid/check -->
-          <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
-          </svg>
-          Thêm Mới
-        </button>
-      </a>
      
         `;
     },
